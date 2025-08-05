703979b1b50e5330e0fd94c4ce0392f8ac94b93b
CHANN KIMLONG <kimlong5244@gmail.com>
2025-08-05 16:14:03 +0700

    test ci/cd

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <h1 className="text-3xl font-bold flex justify-center items-center h-screen">
-      Welcome - merged from test branch
+      Welcome - merged from test branch test again
     </h1>
   )
 }
