fe16cd4a38dd699b824edf49fd039ac06e3b6efa
CHANN KIMLONG <kimlong5244@gmail.com>
2025-08-05 15:52:42 +0700

    test ci/cd

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <h1 className="text-3xl font-bold flex justify-center items-center h-screen">
-      Welcome - test CI/CD with github action
+      Welcome - test
     </h1>
   )
 }
