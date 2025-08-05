00d71beff3339d4d126adbe119d3e109daae60e6
CHANN KIMLONG <kimlong5244@gmail.com>
2025-08-05 15:20:13 +0700

    fix: update text

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <h1 className="text-3xl font-bold flex justify-center items-center h-screen">
-      Hello world! from Chann Kimlong
+      Welcome
     </h1>
   )
 }
