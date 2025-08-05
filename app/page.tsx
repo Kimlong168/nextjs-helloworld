00ee922a2a77e13587b2583725565ad2b3d5f4d1
CHANN KIMLONG <kimlong5244@gmail.com>
2025-08-05 15:01:19 +0700

    fix: update text

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world! dev
+    <h1 className="text-3xl font-bold flex justify-center items-center h-screen">
+      Hello world! from Chann Kimlong
     </h1>
   )
 }
